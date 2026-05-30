2b41b710ae60f5bd37a3da617aa23e7b1b89b901
zurozira <cyrildebui@gmail.com>
2026-05-29 21:31:00 -0600

    Update

--- src/zuroWa.Web/obj/Debug/net10.0/zuroWa.Web.AssemblyInfo.cs
+++ src/zuroWa.Web/obj/Debug/net10.0/zuroWa.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zuroWa.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5745688f5d5a32e7d1a651d8e0930a667ea16b33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a62b1d6062ee24ef2ebaa17e9f66dd297c7cedc")]
 [assembly: System.Reflection.AssemblyProductAttribute("zuroWa.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zuroWa.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
